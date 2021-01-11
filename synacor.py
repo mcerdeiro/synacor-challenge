c07a28e020fafd501acbaee271a07e4667ca3e56
Mariano Cerdeiro <mcerdeiro@gmail.com>
2021-01-11 22:42:36 +0100

    indicate the commands, f1, f9 and f12

--- synacor.py
+++ synacor.py
@@ -8,7 +8,11 @@ from mycpu import memorydumping
 from mycpu import debug
 from mycpu import arrows
 
-
+print("""
+F1  Starts the debugger console
+F9  Loads the memory, stack and register state from stored.bin
+F12 Stores the current state of memory, stack and register into stored.bin
+""")
 cpu = mycpu.MyCpu()
 memdump = memorydumping.MemoryDummping(cpu)
 dbg = debug.Debug(cpu, memdump)
